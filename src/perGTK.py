9bcb631f873cafb85d03ba9bd43d33166923da34
DanilChechkov <danilchechkov@icloud.com>
2020-06-27 20:11:58 +0300

    pkexec

--- src/perGTK.py
+++ src/perGTK.py
@@ -4,6 +4,9 @@ import os
 import sys
 import subprocess
 
+
+#subprocess.Popen(['pkexec','python3'])
+
 gi.require_version("Gtk","3.0")
 from gi.repository import Gtk
 from os.path import abspath
@@ -38,7 +41,7 @@ class auth:
         self.window_is_hidden = False
     
     def passins(self,*args):
-        p = subprocess.Popen(['sudo', '-S'] + ['python3'] + sys.argv, stdin=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
+        p = subprocess.Popen(['sudo', '-A'] + ['python3'] + sys.argv, stdin=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
         p.communicate(pasbox.get_text() + '\n')
         sys.exit(0)
 
@@ -67,29 +70,48 @@ class Handler:
 ar = open('/sys/devices/system/cpu/cpu0/cpufreq/scaling_available_frequencies').readlines()[0].split()
 ar.sort()
 
-if os.getuid() !=0: 
-    pb = Gtk.Builder()
-    pb.add_from_file(path+'PassDi.glade')
-    pb.connect_signals(auth())
-    paswin = pb.get_object('passwin')
-    pasbox = pb.get_object('passbox')
-    pasbut = pb.get_object('button1')
+builder = Gtk.Builder()
+builder.add_from_file(path+'GTKper.glade')
 
-    paswin.connect('destroy',Gtk.main_quit)
-    paswin.show_all()
-    Gtk.main()
-else:
-    builder = Gtk.Builder()
-    builder.add_from_file(path+'GTKper.glade')
+builder.connect_signals(Handler())
 
-    builder.connect_signals(Handler())
+window = builder.get_object('window1')
+sw1 = builder.get_object('sw1')
+sw2 = builder.get_object('sw2')
+sw3 = builder.get_object('sw3')
+sw2.set_active(True)
 
-    window = builder.get_object('window1')
-    sw1 = builder.get_object('sw1')
-    sw2 = builder.get_object('sw2')
-    sw3 = builder.get_object('sw3')
-    sw2.set_active(True)
+window.connect('destroy',Gtk.main_quit)
+window.show_all()
+Gtk.main()
 
-    window.connect('destroy',Gtk.main_quit)
-    window.show_all()
-    Gtk.main()
+#if os.getuid() !=0: 
+    #lnAPP = 'pkexec env DISPLAY="$DISPLAY" XAUTHORITY="$XAUTHORITY"'.split()
+    #lnAPP.append(path+"perGTK.py")
+    #subprocess.Popen(lnAPP)
+    #pb = Gtk.Builder()
+    #pb.add_from_file(path+'PassDi.glade')
+    #pb.connect_signals(auth())
+    #paswin = pb.get_object('passwin')
+    #pasbox = pb.get_object('passbox')
+    #pasbut = pb.get_object('button1')
+
+    #paswin.connect('destroy',Gtk.main_quit)
+    #paswin.show_all()
+    #Gtk.main()
+#else:
+    #subprocess.Popen(['pkexec','python3'])
+    #builder = Gtk.Builder()
+    #builder.add_from_file(path+'GTKper.glade')
+
+    #builder.connect_signals(Handler())
+
+    #window = builder.get_object('window1')
+    #sw1 = builder.get_object('sw1')
+    #sw2 = builder.get_object('sw2')
+    #sw3 = builder.get_object('sw3')
+    #sw2.set_active(True)
+
+    #window.connect('destroy',Gtk.main_quit)
+    #window.show_all()
+    #Gtk.main()
